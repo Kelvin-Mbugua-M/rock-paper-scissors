b3b079ec0daec7d66984e487277895ae9046d908
Kelvin-Mbugua-M <mbuguakelvinmunyui@.com>
2024-06-13 12:54:05 +0300

    the pace is getting worse with time

--- script.js
+++ script.js
@@ -12,8 +12,11 @@ let gameOver=document.getElementById('gameOutCome');
 let gameOverClose = document.getElementById('close');
 let resultStatement = document.getElementById('gameOutComeInfo')
 let sessionCount=0;
-let checkerEngine,checkerUser,sessionOn = false,initial;
+let checkerEngine,hisScore,updateScore,checkerUser,sessionOn = false,initial=0,userScoreCounter=0,EngineScoreCounter=0,hide = 2500;
 
+
+engineScore.innerHTML=`The computer score : ${EngineScoreCounter} of 20`;
+personalScore.innerHTML=`Your current score : ${userScoreCounter} of 20`;
 function gameResultHide(){
     gameOver.style.visibility= 'hidden'
     gameOver.style.zIndex = ''
@@ -21,7 +24,7 @@ function gameResultHide(){
 function gameResultShow(){
     gameOver.style.visibility= 'visible'
     gameOver.style.zIndex = ''
-    setTimeout(gameResultHide,3500);
+    setTimeout(gameResultHide,hide);
 }
 function checkWinner(){
     resultStatement.innerHTML="";
@@ -29,28 +32,36 @@ function checkWinner(){
         resultStatement.innerHTML='DRAW';
         resultStatement.style.color='green';
         yourSelectionOutput.innerHTML
+        EngineScoreCounter+=0.5;
+        userScoreCounter+=0.5;
         setTimeout(gameResultShow,1500);
     }
     else if(checkerEngine=='rock' && checkerUser=='paper'){
         resultStatement.innerHTML='YOU WIN';
         resultStatement.style.color='aquamarine';
+        userScoreCounter++;
         setTimeout(gameResultShow,1500);
     }
     else if(checkerEngine=='scissors' && checkerUser=='rock'){
         resultStatement.innerHTML='YOU WIN';
         resultStatement.style.color='aquamarine';
+        userScoreCounter++;
         setTimeout(gameResultShow,1500);
     }
     else if(checkerEngine=='paper' && checkerUser=='scissors'){
         resultStatement.innerHTML='YOU WIN';
         resultStatement.style.color='aquamarine';
+        userScoreCounter++;
         setTimeout(gameResultShow,1500);
+        
     }
     else{
         resultStatement.innerHTML="YOU LOOSE";
         resultStatement.style.color='red';
+        EngineScoreCounter++;
         setTimeout(gameResultShow,1500);
         }
+    // keepScore()
 }
 function engineSelection(){
     let randomSelection = Math.floor(Math.random()*4);
@@ -130,7 +141,87 @@ function checkSession(){
     }
     else return null
 }
+function stopSession(){
+    sessionCount = 0;
+    EngineScoreCounter=0;
+    userScoreCounter=0;
+    if(sessionCount>=15){ 
+        sessionEnd()
+        // return sessionOn= false
+    }
+    else return null
+}
+function keepScore(){
+    if(EngineScoreCounter>=10.5){
+        if(userScoreCounter>=10){
+            sessionEnd()
+            stopSession()
+        }
+        resultStatement.innerHTML=`YOU LOOSE THE SESSION ${EngineScoreCounter}`;
+        resultStatement.style.color='red';
+        EngineScoreCounter++;
+        setTimeout(gameResultShow,1500);
+        // setTimeout(sessionEnd,1500)
+        // setTimeout(stopSession,1500)
+        return hide=5500;
+    }
+    else if(userScoreCounter>=10.5){
+        if(EngineScoreCounter>=10){
+            sessionEnd()
+            stopSession()
+        }
+        resultStatement.innerHTML=`YOU WON WITH ${userScoreCounter}`;
+        resultStatement.style.color='green';
+        EngineScoreCounter++;
+        setTimeout(gameResultShow,1500);
+        // setTimeout(sessionEnd,300)
+        // setTimeout(stopSession,1500)
+        return hide=5500
+    }
+    else return null
+}
+
+function updateHistoryScore(){
+    try{
+        updateScore=localStorage.getItem('hisScore');
+        historyScore=JSON.parse(updateScore);
+    }
+    catch(e){
+    
+       updateScore = localStorage.setItem('highScore',1);
+    }
+}
+document.addEventListener('keypress',function(e){
+    if(e.key==' '||e.key=='enter'){
+        if(sessionOn==false){
+            sessionStart()
+        }
+        else if (sessionOn==true){
+            sessionEnd()
+        }
+        else return null
+    }
+    if(e.key=="r"){
+        if(sessionOn==true){
+            itemSelection('ro')
+        }
+        else return null
+    }
+    if(e.key=="p"){
+        if(sessionOn==true){
+            itemSelection('pa')
+        }
+        else return null
+    }
+    if(e.key=="s"){
+        if(sessionOn==true){
+            itemSelection('sc')
+        }
+        else return null
+    }
+})
 
 gameOverClose.addEventListener('click', gameResultHide);
 activateSession.addEventListener('click',checkSession);
 window.onload = sessionEnd;
+window.onload=keepScore;
